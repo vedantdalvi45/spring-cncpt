6cbc8a7106326cd5b396907bab4165c4c347ed0c
1pcve <vedalvi16@gmail.com>
2025-07-01 19:14:02 +0530

    mod in component-scan

--- Spring Core/component-scan/src/main/java/com/example/demo/controller/API.java
+++ Spring Core/component-scan/src/main/java/com/example/demo/controller/API.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
