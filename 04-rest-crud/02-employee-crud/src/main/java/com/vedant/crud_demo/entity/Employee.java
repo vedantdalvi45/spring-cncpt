b5379caa78d487cec63132a43f45f9cdd9b322ab
1pcve <vedalvi16@gmail.com>
2025-07-31 17:03:29 +0530

    Added Employee Entity

---
+++ 04-rest-crud/02-employee-crud/src/main/java/com/vedant/crud_demo/entity/Employee.java
@@ -0,0 +1,40 @@
+package com.vedant.crud_demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ConditionalOnIssuerLocationJwtDecoder;
+
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    public Employee(){}
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
