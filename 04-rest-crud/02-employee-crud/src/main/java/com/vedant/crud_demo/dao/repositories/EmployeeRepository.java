f84882e6899aa413e32f44c96e4a8d0d89091ae0
1pcve <vedalvi16@gmail.com>
2025-08-01 18:59:47 +0530

    initial

--- 04-rest-crud/02-employee-crud/src/main/java/com/vedant/crud_demo/dao/repositories/EmployeeRepository.java
+++ 04-rest-crud/02-employee-crud/src/main/java/com/vedant/crud_demo/dao/repositories/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.vedant.crud_demo.dao.repositories;
 
 import com.vedant.crud_demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 }
