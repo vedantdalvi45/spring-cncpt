b5101481a0f92ec2c9a2ff9a74463894b683dcc7
= <=>
2025-08-17 01:04:58 +0530

    added : google auth

---
+++ 05-rest-security/05-user-scrty-jdbc-postgreSQL-jwt-0Auth2/src/test/java/com/ved/OAuth2/Demo/OAuth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ved.OAuth2.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
